f9184db1bd0f62d6e795522d3f40bbe2e01ac569
Jake Archibald <jaffathecake@gmail.com>
2014-08-11 10:52:20 +0100

    Using idb for page storage

--- worker.js
+++ worker.js
@@ -1,37 +1,63 @@
-this.storedURLs = {};
+importScripts('idb.js');
+
+function getDB() {
+  return new IDBHelper('sw-wiki', 1, function(db, oldVersion) {
+    if (oldVersion < 1) {
+      db.createObjectStore('pages');
+    }
+  });
+}
+
+this.addEventListener('install', function(event) {
+  event.waitUntil(getDB().ready);
+});
 
 this.addEventListener('fetch', function(e) {
-  var blobHTML;
-  
-  if(/\/savepage\?/.test(e.request.url)){
-      var params = e.request.url.match(/\?(.+)$/)[1].split("&");
-      var req_url = decodeURIComponent(params[0].split("=")[1]);
-      var req_body = decodeURI(params[1].split("=")[1]).replace(/\+/g," ");
-      this.storedURLs[req_url] = req_body;
-      blobHTML = "<h2>done!</h2>";    
-  }else{
-    if(this.storedURLs[e.request.url]){
-      blobHTML = this.storedURLs[e.request.url];
-    }else {
-      blobHTML = "<h2>New page</h2><form action='/savepage'><input name='url' " + 
-                 "value='"+e.request.url+"' readonly><br/><textarea rows='30' cols='80'" +
-                 "name='body' required></textarea><br/><input type='submit'></form>";
-    }
-  }
+  var idb = getDB();
 
-  Object.keys(this.storedURLs).forEach(function (url,i) {
-    if(i===0) blobHTML += "<h2>Existing Pages</h2>";
-    blobHTML += "<a href='"+url+"'>"+url+"</a><br/>";
-  });
-  
-  blobHTML += "<h2>Create a new page</h2>"
-  blobHTML += "Point the browser to a new URL within this domain";
-  
-  blobHTML += "<h2>Remove the service worker</h2>"
-  blobHTML += "Visit: chrome://serviceworker-internals/";
+  e.respondWith(
+    new Promise(function(resolve) {
+      if(/\/savepage\?/.test(e.request.url)){
+          var params = e.request.url.match(/\?(.+)$/)[1].split("&");
+          var req_url = decodeURIComponent(params[0].split("=")[1]);
+          var req_body = decodeURI(params[1].split("=")[1]).replace(/\+/g," ");
+          resolve(
+            idb.put('pages', req_url, req_body).then(function() {
+              return "<h2>done!</h2>";
+            })
+          );
+      }else{
+        resolve(
+          idb.get('pages', e.request.url).then(function(blobHTML) {
+            if (blobHTML) {
+              return blobHTML;
+            }
+            return "<h2>New page</h2><form action='./savepage'><input name='url' " +
+                   "value='"+e.request.url+"' readonly><br/><textarea rows='30' cols='80'" +
+                   "name='body' required></textarea><br/><input type='submit'></form>";
+          })
+        );
+      }
+    }).then(function(blobHTML) {
+      var i = 0;
 
-  e.respondWith(new Response(new Blob([blobHTML], {type : 'text/html'}), {
-    headers: {"Content-Type": "text/html"}
-  }));
+      return idb.each('pages', function(data, url) {
+        if(i===0) blobHTML += "<h2>Existing Pages</h2>";
+        blobHTML += "<a href='"+url+"'>"+url+"</a><br/>";
+        i++;
+      }).then(function() {
+        return blobHTML;
+      });
+    }).then(function(blobHTML) {
+      blobHTML += "<h2>Create a new page</h2>";
+      blobHTML += "Point the browser to a new URL within this domain";
+      
+      blobHTML += "<h2>Remove the service worker</h2>";
+      blobHTML += "Visit: chrome://serviceworker-internals/";
 
+      return new Response(new Blob([blobHTML], {type : 'text/html'}), {
+        headers: {"Content-Type": "text/html"}
+      });
+    })
+  );
 }.bind(this));
